f33cf6d82debb41f4eb5e25fd65daa3a46e7f1b2
Karina Hernandez Gamino <k.hernandex.g@gmail.com>
2025-08-28 20:48:28 -0700

    Added test cases to main.cpp

--- main.cpp
+++ main.cpp
@@ -5,5 +5,15 @@ using namespace std;
 
 int main() {
     // Add test cases here
+     // Testing the int instance
+    Pair<int> intPair(1, 2);
+
+    // Testing the string instance
+    Pair<string> stringPair("apple", "banana");
+
+    // Output Results
+    cout << "The int pair is: " << intPair.getFirst() << " and " << intPair.getSecond() << endl;
+    cout << "The string pair is: " << stringPair.getFirst() << " and " << stringPair.getSecond() << endl;
+
     return 0;
 }
