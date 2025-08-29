62499434eaee55422ca49bddca8225fde2be3bfd
Karina Hernandez Gamino <k.hernandex.g@gmail.com>
2025-08-28 20:50:23 -0700

    fixed the include directive and added the class template declarations.

--- Pair.cpp
+++ Pair.cpp
@@ -2,7 +2,7 @@
 // Karina Hernandez, Harman Nagra, Alejandro Yepez
 // Implement member functions here
 
-#include <"Pair.h">
+#include "Pair.h"
 
 template <typename T>
 Pair <T>::Pair(T f, T s) {
@@ -10,18 +10,22 @@ Pair <T>::Pair(T f, T s) {
 	s = second;
 }
 
+template <typename T>
 T Pair<T>::getFirst() const {
 	return first;
 }
 
+template <typename T>
 T Pair<T>::getSecond() const {
 	return second;
 }
 
+template <typename T>
 void Pair<T>::setFirst(T f) {
 	first = f;
 }
 
+template <typename T>
 void Pair<T>::setSecond(T s) {
 	second = s;
 }
